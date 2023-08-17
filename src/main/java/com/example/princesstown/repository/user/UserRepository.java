e6755ab572dd3e0e84bfaf2ae7f658425e163fbe
insu <insubono@gmail.com>
2023-08-17 21:13:26 +0900

    <Repository> UserRepository init commit

--- src/main/java/com/example/princesstown/repository/user/UserRepository.java
+++ src/main/java/com/example/princesstown/repository/user/UserRepository.java
@@ -1,4 +1,13 @@
 package com.example.princesstown.repository.user;
 
-public interface UserRepository {
+import com.example.princesstown.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User,Long> {
+
+    Optional<User> findByUsername (String username);
+
+    Optional<User> findByEmail(String email);
 }
