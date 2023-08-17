f9eafb451302a936e2281f45677223bceeefa73e
MSM <qw12345611@naver.com>
2023-08-17 19:56:13 +0900

    3rd commit(Before service development)

--- src/main/java/com/example/princesstown/controller/comment/CommentController.java
+++ src/main/java/com/example/princesstown/controller/comment/CommentController.java
@@ -2,6 +2,7 @@ package com.example.princesstown.controller.comment;
 
 import com.example.princesstown.dto.comment.CommentRequestDto;
 import com.example.princesstown.dto.comment.RestApiResponseDto;
+import com.example.princesstown.exception.TokenNotValidateException;
 import com.example.princesstown.security.user.UserDetailsImpl;
 import com.example.princesstown.service.comment.CommentService;
 import lombok.RequiredArgsConstructor;
@@ -16,16 +17,66 @@ public class CommentController {
 
     private final CommentService commentService;
 
-    // 댓글 작성
-//    @PostMapping("/posts/{postId}/coomments")
-//    public ResponseEntity<RestApiResponseDto> createComment(
-//            @PathVariable Long postId,
-//            @RequestBody CommentRequestDto requestDto,
-//            @AuthenticationPrincipal UserDetailsImpl userDetails) {
-//        this.tokenValidate(userDetails);
-//
-//        return commentService.createComment(postId, requestDto, userDetails.getUser());
-//    }
+    @GetMapping("/posts/{postId}/coomments")
+    public ResponseEntity<RestApiResponseDto> getComments(
+            @PathVariable Long postId,
+            @RequestBody CommentRequestDto requestDto
+    ) {
+        return commentService.getComments(postId, requestDto);
+    }
 
+     // 댓글 작성
+    @PostMapping("/posts/{postId}/coomments")
+    public ResponseEntity<RestApiResponseDto> createComments(
+            @PathVariable Long postId,
+            @RequestBody CommentRequestDto requestDto,
+            @AuthenticationPrincipal UserDetailsImpl userDetails
+    ) {
+        this.tokenValidate(userDetails);
 
+        return commentService.createComments(postId, requestDto, userDetails.getUser());
+    }
+
+    // 댓글 수정
+    @PutMapping("/posts/{postId}/comments/{commentId}")
+    public ResponseEntity<RestApiResponseDto> updateComments(
+            @PathVariable Long postId,
+            @PathVariable Long commentId,
+            @AuthenticationPrincipal UserDetailsImpl userDetails
+    ) {
+        this.tokenValidate(userDetails);
+
+        return commentService.updateComments(postId, commentId, userDetails.getUser());
+    }
+
+    // 댓글 삭제
+    @DeleteMapping("/posts/{postId}/comments/{commentId}")
+    public ResponseEntity<RestApiResponseDto> deleteComments(
+            @PathVariable Long postId,
+            @PathVariable Long commentId,
+            @AuthenticationPrincipal UserDetailsImpl userDetails
+    ) {
+        this.tokenValidate(userDetails);
+
+        return commentService.deleteComments(postId, commentId, userDetails.getUser());
+    }
+
+    @PutMapping("/posts/{postId}/comments/{commentId}/likes")
+    public ResponseEntity<RestApiResponseDto> likestogle(
+            @PathVariable Long postId,
+            @PathVariable Long commentId,
+            @AuthenticationPrincipal UserDetailsImpl userDetails
+    ) {
+        this.tokenValidate(userDetails);
+
+        return commentService.liketogle(postId, commentId, userDetails.getUser());
+    }
+
+    public void tokenValidate(UserDetailsImpl userDetails) {
+         try {
+             userDetails.getUser();
+         } catch (Exception e) {
+             throw new TokenNotValidateException("토큰이 유효하지 않습니다.");
+         }
+    }
 }
--- src/main/java/com/example/princesstown/service/comment/CommentService.java
+++ src/main/java/com/example/princesstown/service/comment/CommentService.java
@@ -1,9 +1,60 @@
 package com.example.princesstown.service.comment;
 
+import com.example.princesstown.dto.comment.CommentRequestDto;
+import com.example.princesstown.dto.comment.RestApiResponseDto;
+import com.example.princesstown.repository.comment.CommentRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
 public class CommentService {
+
+    private final CommentRepository commentRepository;
+
+    public ResponseEntity<RestApiResponseDto> getComments(Long postId, CommentRequestDto requestDto) {
+        try {
+
+        } catch (IllegalArgumentException e) {
+
+        }
+        return null;
+    }
+
+    public ResponseEntity<RestApiResponseDto> createComments(Long postId, CommentRequestDto requestDto, userDetails) {
+        try {
+
+        } catch (IllegalArgumentException e) {
+
+        }
+        return null;
+    }
+
+    public ResponseEntity<RestApiResponseDto> updateComments(Long postId, CommentRequestDto requestDto, userDetails) {
+        try {
+
+        } catch (IllegalArgumentException e) {
+
+        }
+        return null;
+    }
+
+    public ResponseEntity<RestApiResponseDto> deleteComments(Long postId, CommentRequestDto requestDto, userDetails) {
+        try {
+
+        } catch (IllegalArgumentException e) {
+
+        }
+        return null;
+    }
+
+    public ResponseEntity<RestApiResponseDto> liketogle(Long postId, CommentRequestDto requestDto, userDetails) {
+        try {
+
+        } catch (IllegalArgumentException e) {
+
+        }
+        return null;
+    }
 }
