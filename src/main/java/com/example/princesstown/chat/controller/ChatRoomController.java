3f42b3f879c7777137df092eb299f4e1a5a1d47b
Minji-Sohn2 <cyzh1311@gmail.com>
2023-08-18 11:55:29 +0900

    :art: 채팅방 삭제 리턴 수정

--- src/main/java/com/example/princesstown/chat/controller/ChatRoomController.java
+++ src/main/java/com/example/princesstown/chat/controller/ChatRoomController.java
@@ -9,6 +9,7 @@ import com.example.princesstown.dto.response.ApiResponseDto;
 import com.example.princesstown.security.user.UserDetailsImpl;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
@@ -49,7 +50,7 @@ public class ChatRoomController {
     ) {
         log.info("채팅방 삭제 컨트롤러 -> " + userDetails.getUser().getUsername());
         chatRoomService.deleteChatRoom(roomId, userDetails.getUser());
-        return null;
+        return ResponseEntity.ok().body(new ApiResponseDto(HttpStatus.OK.value(), "채팅방 삭제 완료"));
     }
 
     @GetMapping("/rooms/{roomId}/members")
