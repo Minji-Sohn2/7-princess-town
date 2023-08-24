e1ba3aa69f54e2f43ac45794ff52b67a718ba8b4
insu <insubono@gmail.com>
2023-08-24 18:25:45 +0900

    <Entity> Location,User,Post 엔티티 필드 추가

--- src/main/java/com/example/princesstown/entity/Post.java
+++ src/main/java/com/example/princesstown/entity/Post.java
@@ -1,4 +1,68 @@
+
 package com.example.princesstown.entity;
 
-public class Post {
+import com.example.princesstown.dto.request.PostRequestDto;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+@Table(name = "post")
+@NoArgsConstructor
+public class Post extends Timestamped { // 상속받아서 createdAt, modifiedAt column 가져옴
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String title;
+
+    @Column(nullable = false, length = 3000)
+    private String contents;
+
+    @Column(nullable = false)
+    private String nickname;
+
+    @Column(nullable = false)
+    private String username;
+
+    @Column(nullable = false)
+    private Integer postLikeCount;
+
+    @JsonIgnore
+    @ManyToOne
+    @JoinColumn(name="user_id", nullable = false)
+    private User user;
+
+    @ManyToOne
+    @JoinColumn(name = "location_id")
+    private Location location;
+
+//    @OneToMany( mappedBy = "post",fetch = FetchType.EAGER, cascade = CascadeType.ALL)
+//    private List<Comment> commentList = new ArrayList<>();
+
+    public Post(PostRequestDto requestDto, User user) { //
+        this.title = requestDto.getTitle();
+        this.contents = requestDto.getContents();
+        this.nickname = user.getNickname();
+        this.username = user.getUsername();
+        this.postLikeCount = 0;
+        this.user = user;
+    }
+
+
+    public void updatePost(PostRequestDto postRequestDto) {
+        this.title = postRequestDto.getTitle();
+        this.contents = postRequestDto.getContents();
+        this.username = user.getUsername();
+        this.nickname = user.getNickname();
+    }
+
+    public void setPostLikedCount(Integer postLikedCount) {
+        this.postLikeCount = postLikedCount;
+    }
 }
