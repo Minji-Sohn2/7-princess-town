6c9729438236628d0d56b222c013130a98c9f36a
insu <insubono@gmail.com>
2023-08-28 19:35:46 +0900

    <Repository> auth2 repository 추가 및 UserRepository 메서드 추가

--- src/main/java/com/example/princesstown/repository/user/UserRepository.java
+++ src/main/java/com/example/princesstown/repository/user/UserRepository.java
@@ -4,17 +4,17 @@ import com.example.princesstown.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
-import org.springframework.stereotype.Repository;
 
 import java.util.List;
-import java.util.Map;
 import java.util.Optional;
 
 public interface UserRepository extends JpaRepository<User,Long> {
 
-    Optional<User> findByUsername (String username);
+    Optional<User> findByUsername(String username);
 
-    Optional<User> findByEmail(String email);
+    Optional<User> findBynickname(String nickname);
+
+    User findByEmail(String email);
 
     @Query("SELECT u FROM User u WHERE u.username = :username AND u.email = :email")
     Optional<User> emailCheck(@Param("username") String username, @Param("email") String email);
