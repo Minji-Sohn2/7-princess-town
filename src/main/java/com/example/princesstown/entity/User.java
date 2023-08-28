5faf3d7ae919bd624d76a2427f18645076d6c1b4
insu <insubono@gmail.com>
2023-08-28 19:39:37 +0900

    <Entity> User엔티티 : email 필드 nullable조건 삭제 및 locagion_id컬럼 -> location_locationId로 수정, Post엔티티 : user_id, loacation_id 컬럼 -> user_userId, location_locationId로 수정

--- src/main/java/com/example/princesstown/entity/User.java
+++ src/main/java/com/example/princesstown/entity/User.java
@@ -1,9 +1,13 @@
 package com.example.princesstown.entity;
 
+import com.example.princesstown.dto.getInfo.KakaoUserInfoDto;
+import com.example.princesstown.dto.getInfo.NaverUserInfoDto;
 import com.example.princesstown.dto.request.ProfileEditRequestDto;
 import com.example.princesstown.dto.request.SignupRequestDto;
 import jakarta.persistence.*;
-import lombok.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 @Entity
 @Getter
@@ -24,7 +28,7 @@ public class User {
     @Column(nullable = false, unique = true)
     private String nickname;
 
-    @Column(nullable = false, unique = true)
+    @Column(unique = true)
     private String email;
 
     @Column(nullable = false, unique = true)
@@ -37,15 +41,28 @@ public class User {
     private String PhoneVerifyCode;
 
     @ManyToOne(cascade = {CascadeType.PERSIST, CascadeType.MERGE})
-    @JoinColumn(name = "location_id")
+    @JoinColumn(name = "location_locationId")
     private Location location;
-    public User(SignupRequestDto signupRequestDto, String password) {
+
+    public User(SignupRequestDto signupRequestDto, String encodedPassword) {
         this.username = signupRequestDto.getUsername();
-        this.password = password;
+        this.password = encodedPassword;
         this.nickname = signupRequestDto.getNickname();
         this.email = signupRequestDto.getEmail();
         this.phoneNumber = signupRequestDto.getPhoneNumber();
         this.PhoneVerifyCode = signupRequestDto.getPhoneVerifyCode();
+    }
+
+    public User(KakaoUserInfoDto kakaoUserInfoDto, String encodedPassword) {
+        this.nickname = kakaoUserInfoDto.getNickname();
+        this.password = encodedPassword;
+        this.userId = Long.valueOf(kakaoUserInfoDto.getId());
+    }
+
+    public User(NaverUserInfoDto naverUserInfoDto, String encodedPassword) {
+        this.password = encodedPassword;
+        this.nickname = naverUserInfoDto.getNickname();
+        this.userId = Long.valueOf(naverUserInfoDto.getId());
     }
 
     public void editProfile(ProfileEditRequestDto profileEditRequestDto, String password) {
@@ -55,4 +72,9 @@ public class User {
         this.email = profileEditRequestDto.getEmail();
         this.phoneNumber = profileEditRequestDto.getPhoneNumber();
     }
+
+//    public Optional<User> kakaoIdUpdate(String username) {
+//        this.username = username;
+//        return Optional.of(this);
+//    }
 }
