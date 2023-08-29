47efed12ea95322212ffd17941b12b2c3737f93c
insu <insubono@gmail.com>
2023-08-29 21:17:52 +0900

    <Service> TokenBlacklistService Redis를 이용한 로직 수정

--- src/main/java/com/example/princesstown/service/user/TokenBlacklistService.java
+++ src/main/java/com/example/princesstown/service/user/TokenBlacklistService.java
@@ -1,21 +1,25 @@
 package com.example.princesstown.service.user;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.stereotype.Service;
 
-import java.util.Collections;
-import java.util.HashSet;
-import java.util.Set;
+import java.util.concurrent.TimeUnit;
 
 @Service
+@RequiredArgsConstructor
 public class TokenBlacklistService {
+    private final StringRedisTemplate stringRedisTemplate; // 레디스 템플릿
 
-    private final Set<String> blacklist = Collections.synchronizedSet(new HashSet<>());
+    private final String BLACKLIST_PREFIX = "BLACKLISTED_TOKEN ";
 
+    // 블랙리스트에 토큰 추가
     public void addToBlacklist(String token) {
-        blacklist.add(token);
+        stringRedisTemplate.opsForValue().set(BLACKLIST_PREFIX + token, "", 1, TimeUnit.HOURS); // 1시간 후 만료
     }
 
+    // 블랙리스트에 있는 토큰인지 확인
     public boolean isBlacklisted(String token) {
-        return blacklist.contains(token);
+        return stringRedisTemplate.hasKey(BLACKLIST_PREFIX + token);
     }
 }
