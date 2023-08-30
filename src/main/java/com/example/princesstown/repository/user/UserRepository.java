6d4892901e942d3899d0e1cd76ca202c6c689c18
insu <insubono@gmail.com>
2023-08-30 17:15:33 +0900

    <Repository> UserRepository 안쓰는 메서드 삭제 및 다른 메서드 추가

--- src/main/java/com/example/princesstown/repository/user/UserRepository.java
+++ src/main/java/com/example/princesstown/repository/user/UserRepository.java
@@ -2,10 +2,7 @@ package com.example.princesstown.repository.user;
 
 import com.example.princesstown.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 
-import java.util.List;
 import java.util.Optional;
 
 public interface UserRepository extends JpaRepository<User,Long> {
@@ -16,14 +13,7 @@ public interface UserRepository extends JpaRepository<User,Long> {
 
     User findByEmail(String email);
 
-    @Query("SELECT u FROM User u WHERE u.username = :username AND u.email = :email")
-    Optional<User> emailCheck(@Param("username") String username, @Param("email") String email);
-
-    @Query("SELECT u FROM User u WHERE u.username = :username AND u.phoneNumber = :phoneNumber")
-    Optional<User> phoneCheck(@Param("username") String username, @Param("phoneNumber") String phoneNumber);
-
-    @Query("SELECT u.username FROM User u WHERE u.email = :email")
-    List<String> findIdByEmail(@Param("email") String email);
-
     Optional<User> findByPhoneNumberAndEmail(String phoneNumber, String email);
+
+    User findByPhoneNumber(String phoneNumber);
 }
