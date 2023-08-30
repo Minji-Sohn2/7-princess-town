84551ace2737ad99075bcb9f33a9b36410622d24
insu <insubono@gmail.com>
2023-08-30 17:14:22 +0900

    <Service> UserService 휴대폰 인증 로직 추가 및 회원가입 로직 수정

--- src/main/java/com/example/princesstown/service/user/UserService.java
+++ src/main/java/com/example/princesstown/service/user/UserService.java
@@ -10,12 +10,11 @@ import com.example.princesstown.security.jwt.JwtUtil;
 import com.example.princesstown.service.awsS3.S3Uploader;
 import com.example.princesstown.service.email.MailService;
 import com.example.princesstown.service.message.MessageService;
-import jakarta.servlet.http.HttpServletRequest;
-import jakarta.servlet.http.HttpSession;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.ApplicationContext;
+import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
@@ -23,8 +22,6 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import java.io.IOException;
-import java.util.HashMap;
-import java.util.Map;
 import java.util.Optional;
 import java.util.regex.Pattern;
 
@@ -42,12 +39,19 @@ public class UserService {
     private final AuthenticationManager authenticationManager;
     private final MessageService messageService;
     private final ApplicationContext applicationContext;
-    public Map<String, Object> authNumMap = new HashMap<>();
+    private final StringRedisTemplate redisTemplate;
 
-    public ResponseEntity<ApiResponseDto> signup(SignupRequestDto requestDto, HttpServletRequest request) {
+
+    public ResponseEntity<ApiResponseDto> signup(SignupRequestDto requestDto) {
+        // 휴대폰 인증 검사
+        ResponseEntity<ApiResponseDto> phoneVerificationResponse = messageService.verifyCode(requestDto.getPhoneNumber(), requestDto.getPhoneVerifyCode());
+
+        if (phoneVerificationResponse.getStatusCode() != HttpStatus.OK) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(new ApiResponseDto(HttpStatus.BAD_REQUEST.value(), "휴대폰 인증 실패"));
+        }
+
         String username = requestDto.getUsername();
         String notEncodingPassword = requestDto.getPassword();
-        String getPhoneVerifyCode = requestDto.getPhoneVerifyCode();
 
         // user 테이블에 입력받은 username과 동일한 데이터가 있는지 확인
         Optional<User> checkUser = userRepository.findByUsername(username);
@@ -75,53 +79,44 @@ public class UserService {
             return ResponseEntity.badRequest().body(new ApiResponseDto(HttpStatus.BAD_REQUEST.value(), "비밀번호는 최소 8자 이상, 15자 이하이며 알파벳 대소문자, 숫자로 구성되어야 합니다."));
         }
 
-        // 휴대폰 인증 코드 검증
-        HttpSession session = request.getSession();
-        String storedCode = (String) session.getAttribute(requestDto.getPhoneNumber());
-
-        if (storedCode == null) {
-            return ResponseEntity.status(400).body(new ApiResponseDto(HttpStatus.BAD_REQUEST.value(), "인증 번호가 만료되었거나 없습니다."));
+        // 중복 전화번호 검증
+        String phoneNumber = requestDto.getPhoneNumber();
+        User checkPhoneNumber = userRepository.findByPhoneNumber(phoneNumber);
+        if(checkPhoneNumber != null) {
+            return ResponseEntity.badRequest().body(new ApiResponseDto(HttpStatus.BAD_REQUEST.value(), "중복된 전화번호 입니다."));
         }
 
-        if (!getPhoneVerifyCode.equals(storedCode)) {
-            return ResponseEntity.badRequest().body(new ApiResponseDto(HttpStatus.BAD_REQUEST.value(), "휴대폰 인증 코드가 올바르지 않습니다."));
-        }
+            // 비밀번호 인코딩
+            String encodedPassword = passwordEncoder.encode(notEncodingPassword);
 
-        // 인증 성공한 경우, 세션에서 인증 번호 삭제
-        session.removeAttribute(requestDto.getPhoneNumber());
+            // 회원 가입 진행
+            // requestDto.profileImage를 제외한 필드가 객체에 저장이 됨
+            User user = new User(requestDto, encodedPassword);
 
+            // S3에 이미지 업로드
+            try {
+                String imageUrl;
 
-        // 비밀번호 인코딩
-        String encodedPassword = passwordEncoder.encode(notEncodingPassword);
+                if (requestDto.getProfileImage() != null) {
+                    // S3에 이미지를 업로드하고, 이미지 URL을 받아와서 user.profileImage에 직접 객체에 저장이 됨
+                    imageUrl = s3Uploader.upload(requestDto.getProfileImage(), "profile-images");
+                } else {
+                    // 이미지가 없는 경우 기본 이미지 URL을 설정
+                    imageUrl = s3Uploader.uploadDefaultImage(applicationContext);
+                }
+                // 이미지 URL을 설정
+                user.setProfileImage(imageUrl);
 
-        // 회원 가입 진행
-        // requestDto.profileImage를 제외한 필드가 객체에 저장이 됨
-        User user = new User(requestDto, encodedPassword);
-
-        // S3에 이미지 업로드
-        try {
-            String imageUrl;
-
-            if (requestDto.getProfileImage() != null) {
-                // S3에 이미지를 업로드하고, 이미지 URL을 받아와서 user.profileImage에 직접 객체에 저장이 됨
-                imageUrl = s3Uploader.upload(requestDto.getProfileImage(), "profile-images");
-            } else {
-                // 이미지가 없는 경우 기본 이미지 URL을 설정
-                imageUrl = s3Uploader.uploadDefaultImage(applicationContext);
+            } catch (IOException e) {
+                // 로깅 또는 적절한 에러 메시지를 반환
+                return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(new ApiResponseDto(HttpStatus.INTERNAL_SERVER_ERROR.value(), "이미지 업로드 중 오류가 발생했습니다."));
             }
-            // 이미지 URL을 설정
-            user.setProfileImage(imageUrl);
 
-        } catch (IOException e) {
-            // 로깅 또는 적절한 에러 메시지를 반환
-            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(new ApiResponseDto(HttpStatus.INTERNAL_SERVER_ERROR.value(), "이미지 업로드 중 오류가 발생했습니다."));
+            // User 객체 저장
+            userRepository.save(user);
+
+            return ResponseEntity.status(201).body(new ApiResponseDto(HttpStatus.CREATED.value(), "회원가입 완료 되었습니다."));
         }
-
-        // User 객체 저장
-        userRepository.save(user);
-
-        return ResponseEntity.status(201).body(new ApiResponseDto(HttpStatus.CREATED.value(), "회원가입 완료 되었습니다."));
-    }
 
 
     // 회원 탈퇴
@@ -144,7 +139,7 @@ public class UserService {
         // 위에서 받아온 username과 일치하는 User 가져오기
         Optional<User> checkUser = userRepository.findByUsername(username);
 
-        // DB에 없는 사용자인 경우 혹은 비밀번호가 일치하지 않을 경우
+        // DB에 없는 사용자인 경우 혹은 인코딩되지 않은 임시 비밀번호를 인코딩하여 DB 저장된 인코딩된 임시 비밀번호랑 같지 않을 때
         if (checkUser.isEmpty() || !passwordEncoder.matches(password, checkUser.get().getPassword())) {
 
             log.info(username);
@@ -169,7 +164,8 @@ public class UserService {
 
     // 유저조회
     public ProfileResponseDto lookupUser(Long userId) {
-        return userRepository.findById(userId).map(ProfileResponseDto::new).orElseThrow(
+        return  userRepository.findById(userId)
+                .map(user -> new ProfileResponseDto(user, user.getLocation())).orElseThrow(
                 () -> new IllegalArgumentException("선택한 게시물은 존재하지 않습니다."));
     }
 
