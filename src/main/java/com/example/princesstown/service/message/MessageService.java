0bbf38aedb1a6a0e54e57ff87deba7b6b356a34a
insu <insubono@gmail.com>
2023-08-31 14:27:41 +0900

    <Service> MessageService 메세지 전송 임시 주석 처리

--- src/main/java/com/example/princesstown/service/message/MessageService.java
+++ src/main/java/com/example/princesstown/service/message/MessageService.java
@@ -7,12 +7,18 @@ import net.nurigo.sdk.NurigoApp;
 import net.nurigo.sdk.message.exception.NurigoMessageNotReceivedException;
 import net.nurigo.sdk.message.model.Message;
 import net.nurigo.sdk.message.service.DefaultMessageService;
+import org.json.JSONObject;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
+import java.io.BufferedReader;
+import java.io.DataOutputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
 import java.util.Random;
 import java.util.concurrent.TimeUnit;
 
@@ -21,22 +27,23 @@ import java.util.concurrent.TimeUnit;
 @RequiredArgsConstructor
 public class MessageService {
 
-    @Value("${nurigo.app.key}")
-    private String appKey;
-
-    @Value("${nurigo.app.secret}")
-    private String appSecret;
-
-    @Value("${nurigo.api.url}")
-    private String apiUrl;
-
-    @Value("${phoneNumber}")
-    private String sendPhoneNumber;
+//    @Value("${nurigo.app.key}")
+//    private String appKey;
+//
+//    @Value("${nurigo.app.secret}")
+//    private String appSecret;
+//
+//    @Value("${nurigo.api.url}")
+//    private String apiUrl;
+//
+//    @Value("${phoneNumber}")
+//    private String sendPhoneNumber;
 
     private final StringRedisTemplate redisTemplate;
 
+
     public ResponseEntity<ApiResponseDto> sendVerificationCode(String phoneNumber) {
-        DefaultMessageService messageService = NurigoApp.INSTANCE.initialize(appKey, appSecret, apiUrl);
+//        DefaultMessageService messageService = NurigoApp.INSTANCE.initialize(appKey, appSecret, apiUrl);
 
         Random ran = new Random();
         StringBuilder numStr = new StringBuilder();
@@ -47,20 +54,20 @@ public class MessageService {
 
         // Redis를 사용하여 "key" : phoneNumber, "value" : numStr.toString(), "양" : 5, "단위" : minute로 설정
         redisTemplate.opsForValue().set(phoneNumber, numStr.toString(), 5, TimeUnit.MINUTES); // 5분 후 만료
-
-        Message message = new Message();
-        message.setFrom(sendPhoneNumber);
-        message.setTo(phoneNumber);
-        message.setText(" 인증번호는 [" + numStr + "] 입니다.");
-
-        try {
-            messageService.send(message);
-        } catch (NurigoMessageNotReceivedException exception) {
-            log.error("발송에 실패한 메세지 목록" + exception.getFailedMessageList());
-            log.error("발송한 메세지" + exception.getMessage());
-        } catch (Exception exception) {
-            log.error("발송한 메세지" + exception.getMessage());
-        }
+//
+//        Message message = new Message();
+//        message.setFrom(sendPhoneNumber);
+//        message.setTo(phoneNumber);
+//        message.setText(" 인증번호는 [" + numStr + "] 입니다.");
+//
+//        try {
+//            messageService.send(message);
+//        } catch (NurigoMessageNotReceivedException exception) {
+//            log.error("발송에 실패한 메세지 목록" + exception.getFailedMessageList());
+//            log.error("발송한 메세지" + exception.getMessage());
+//        } catch (Exception exception) {
+//            log.error("발송한 메세지" + exception.getMessage());
+//        }
 
         return ResponseEntity.status(200).body(new ApiResponseDto(HttpStatus.OK.value(), "메세지 전송 성공", null));
     }
