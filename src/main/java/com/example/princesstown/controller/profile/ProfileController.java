fc93893922410353c26802eb2c66a10f2e195ce3
insu <insubono@gmail.com>
2023-09-05 21:08:05 +0900

    <Controller> ProfileController View파일 삭제

--- src/main/java/com/example/princesstown/controller/profile/ProfileController.java
+++ src/main/java/com/example/princesstown/controller/profile/ProfileController.java
@@ -11,22 +11,19 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.multipart.MultipartFile;
-
-import java.io.IOException;
 
 @Slf4j
 @Controller
 @RequiredArgsConstructor
+@RequestMapping("/api/users/profile")
 public class ProfileController {
 
     private final UserService userService;
     private final ProfileService profileService;
 
     // 프로필 조회 메서드
-    @GetMapping("/api/auth/profile")
+    @GetMapping
     @ResponseBody
     public ProfileResponseDto lookupUser(@AuthenticationPrincipal UserDetailsImpl userDetails) {
         Long userId = userDetails.getUser().getUserId();
@@ -36,29 +33,12 @@ public class ProfileController {
     }
 
     // 프로필 수정
-    @PutMapping("/api/auth/profile")
+    @PutMapping
     @ResponseBody
     public ResponseEntity<ApiResponseDto> updateUser(
             @AuthenticationPrincipal UserDetailsImpl userDetails, @ModelAttribute ProfileEditRequestDto editRequestDto) {
         Long userId = userDetails.getUser().getUserId();
 
         return profileService.updateUser(userId, editRequestDto);
-    }
-
-
-    // view.html
-    @GetMapping("/auth/profile")
-    public String getPost(Model model, @AuthenticationPrincipal UserDetailsImpl userDetails) {
-        model.addAttribute("user", userDetails.getUser());
-
-        return "profile";
-    }
-
-    @GetMapping("/update")
-    public String updatePage(Model model, @AuthenticationPrincipal UserDetailsImpl userDetails) {
-        model.addAttribute("username",userDetails.getUser().getUsername());
-        model.addAttribute("user",userDetails.getUser());
-
-        return "profileUpdate";
     }
 }
