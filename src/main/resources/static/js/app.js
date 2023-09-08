2b03e758e70b010ac1fb573f66520556fbafc753
insu <insubono@gmail.com>
2023-09-08 16:30:37 +0900

    <js, html, css> app.js 모달 뜰 시, user menu 숨기기, index.html 반경 직접 입력 삭제

--- src/main/resources/static/js/app.js
+++ src/main/resources/static/js/app.js
@@ -50,14 +50,14 @@ $(document).ready(function() {
 
 		if (token && profileImage) {
 			$('#profile-picture').attr("src", profileImage).show();
-			$('.user.icon').hide();
+			$('#user-icon .user.icon').hide();
 		} else {
-			$('.user.icon').show();
+			$('#user-icon .user.icon').show();
 		}
 
 		if (token && !profileImage) {
 			$('#profile-picture').attr("src", "/img/defaultImg/스프링르탄이.png").show();
-			$('.user.icon').hide();
+			$('#user-icon .user.icon').hide();
 		}
 	}
 
@@ -68,14 +68,83 @@ $(document).ready(function() {
 
 
 	// 모달 초기화
-	const $signupModal = $('#signupModal').modal();
-	const $loginModal = $('#loginModal').modal();
-	const $passwordResetModal = $('#passwordResetModal').modal();
-	const $usernameFindModal = $('#usernameFindModal').modal();
-	const $deactivationModal = $('#deactivationModal').modal();
-	const $logoutModal = $('#logout-confirm-modal').modal();
-	const $profileModal = $('#profileModal').modal();
-	const $deactivationConfirmModal = $('#deactivationConfirmModal').modal();
+	const $signupModal = $('#signupModal').modal({
+		onShow: function() {
+			$('#user-menu').hide();
+		},
+		onHide: function() {
+			$('#user-icon').show();
+		}
+	});
+
+	const $loginModal = $('#loginModal').modal({
+		onShow: function() {
+			$('#user-menu').hide();
+		},
+		onHide: function() {
+			$('#user-icon').show();
+		}
+	});
+
+	const $passwordResetModal = $('#passwordResetModal').modal({
+		onShow: function() {
+			$('#user-menu').hide();
+		},
+		onHide: function() {
+			$('#user-icon').show();
+		}
+	});
+
+	const $usernameFindModal = $('#usernameFindModal').modal({
+		onShow: function() {
+			$('#user-menu').hide();
+		},
+		onHide: function() {
+			$('#user-icon').show();
+		}
+	});
+
+	const $deactivationModal = $('#deactivationModal').modal({
+		onShow: function() {
+			$('#user-menu').hide();
+		},
+		onHide: function() {
+			$('#user-icon').show();
+		}
+	});
+
+	const $logoutModal = $('#logout-confirm-modal').modal({
+		onShow: function() {
+			$('#user-menu').hide();
+		},
+		onHide: function() {
+			$('#user-icon').show();
+		}
+	});
+
+	const $profileModal = $('#profileModal').modal({
+		onShow: function() {
+			$('#user-menu').hide();
+		},
+		onHide: function() {
+			$('#user-icon').show();
+		}
+	});
+
+	const $deactivationConfirmModal = $('#deactivationConfirmModal').modal({
+		onShow: function() {
+			$('#user-menu').hide();
+		},
+		onHide: function() {
+			$('#user-icon').show();
+		}
+	});
+
+// user-icon 버튼 클릭 시 user-menu 토글
+	$('#user-icon').on('click', function() {
+		$('#user-menu').toggle();
+	});
+
 
 	// 페이지 로드 시 '저장' 버튼 숨기기
 	$('#saveProfile').hide();
@@ -907,6 +976,13 @@ $(document).ready(function() {
 		$('#editProfile').hide();
 	});
 
+	// $('#radiusSelect').dropdown({
+	// 	onChange: function (value) {
+	// 		if (value === "custom") {
+	// 			$('#customRadiusInput').show();
+	// 		}
+	// 	}
+	// });
 
 	// 프로필 정보 저장 버튼 클릭 이벤트
 	$('#saveProfile').click(function () {
@@ -920,6 +996,20 @@ $(document).ready(function() {
 		if (newPassword !== "********") {
 			formData.append('password', newPassword);
 		}
+
+		// 반경 설정 값 가져오기
+		var selectedRadiusValue = $('#radiusSelect').val();
+		// var customRadiusInputValue = $('#customRadiusInput').val();
+
+		var finalRadius;
+		if (selectedRadiusValue === 'custom') {
+			finalRadius = customRadiusInputValue; // 직접입력
+		} else {
+			finalRadius = selectedRadiusValue; // 선택값
+		}
+
+
+		formData.append('radius', finalRadius); // 결정된 반경 값 추가
 		formData.append('nickname', $("input[name='profile-nicknameInput']").val());
 		formData.append('email', $("input[name='profile-emailInput']").val());
 		formData.append('phoneNumber', $("input[name='profile-phoneNumberInput']").val());
