88dd97a78cd4a9d146050acc6bfa1689eeb1b77e
insu <insubono@gmail.com>
2023-09-12 12:47:11 +0900

    <Service, Dto, User> NaverService ,NaverUserInfoDto, User phoneNumber 관련 로직 삭제

--- src/main/java/com/example/princesstown/dto/getInfo/NaverUserInfoDto.java
+++ src/main/java/com/example/princesstown/dto/getInfo/NaverUserInfoDto.java
@@ -1,7 +1,5 @@
 package com.example.princesstown.dto.getInfo;
 
-import com.example.princesstown.entity.User;
-import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -10,13 +8,9 @@ import lombok.Setter;
 public class NaverUserInfoDto {
     private String username;
     private String nickname;
-    private String phoneNumber;
 
-
-    public NaverUserInfoDto(String id, String nickname, String phoneNumber) {
+    public NaverUserInfoDto(String id, String nickname) {
         this.username = id;
         this.nickname = nickname;
-        this.phoneNumber = phoneNumber;
     }
-
 }
