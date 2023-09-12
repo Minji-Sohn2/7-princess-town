05d746ca29482bb52c994a9f36431af3b38cd367
insu <insubono@gmail.com>
2023-09-12 19:42:17 +0900

    <Controller> NaverController, KakaoController 위도,경도 부분 Location이 null일 시, 0.0으로 설정하도록 변경

--- src/main/java/com/example/princesstown/controller/auth2/NaverController.java
+++ src/main/java/com/example/princesstown/controller/auth2/NaverController.java
@@ -1,6 +1,7 @@
 package com.example.princesstown.controller.auth2;
 
 import com.example.princesstown.dto.response.ApiResponseDto;
+import com.example.princesstown.entity.Location;
 import com.example.princesstown.entity.User;
 import com.example.princesstown.service.naver.NaverService;
 import lombok.RequiredArgsConstructor;
@@ -33,8 +34,15 @@ public class NaverController {
         String email = userData.getEmail();
         String nickname = userData.getNickname();
         Long userId = userData.getUserId();
-        Double latitude = userData.getLocation().getLatitude();
-        Double longitude = userData.getLocation().getLongitude();
+
+        Location location = userData.getLocation();
+        Double latitude = 0.0; // 기본 값 설정
+        Double longitude = 0.0; // 기본 값 설정
+        if (location != null) {
+            latitude = location.getLatitude();
+            longitude = location.getLongitude();
+        }
+
         log.info("네이버서버에서 보내는 nickname : " + nickname);
         log.info("네이버서버에서 보내는 userId : " + userId);
         log.info("네이버서버에서 보내는 latitude : " + latitude);
