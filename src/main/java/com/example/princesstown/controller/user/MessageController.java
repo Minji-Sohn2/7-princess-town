fb1f66a7ea7ba2762e5367b8c0cbc3f81c917e49
insu <insubono@gmail.com>
2023-09-14 10:22:58 +0900

    <Controller> Find,Message,UserController 쿼리파라미터로 요청받는 컨트롤러 모두 키값을 소문자로 변경

--- src/main/java/com/example/princesstown/controller/user/MessageController.java
+++ src/main/java/com/example/princesstown/controller/user/MessageController.java
@@ -18,13 +18,13 @@ public class MessageController {
 
     //문자 인증번호 발송
     @PostMapping("/codes")
-    public ResponseEntity<ApiResponseDto> sendVerificationCode(@RequestParam("phoneNumber") String phoneNumber) throws Exception {
+    public ResponseEntity<ApiResponseDto> sendVerificationCode(@RequestParam("phonenumber") String phoneNumber) throws Exception {
         return messageService.sendVerificationCode(phoneNumber);
     }
 
     //문자 인증번호 검증
     @PostMapping("verify-codes")
-    public ResponseEntity<ApiResponseDto> verifyCode(@RequestParam("phoneNumber") String phoneNumber, @RequestParam("inputCode") String inputCode) {
+    public ResponseEntity<ApiResponseDto> verifyCode(@RequestParam("phonenumber") String phoneNumber, @RequestParam("inputcode") String inputCode) {
         return messageService.verifyCode(phoneNumber, inputCode);
     }
 }
