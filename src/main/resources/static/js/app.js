57e51d6e6b63b77e6077e93677222261d215eb4d
MSM <qw12345611@naver.com>
2023-09-14 20:14:26 +0900

    54rd commit(카카오맵 추가)

--- src/main/resources/static/js/app.js
+++ src/main/resources/static/js/app.js
@@ -6,7 +6,6 @@ $(document).ready(function () {
 
     // 아이콘 버튼을 클릭할 때 메뉴를 토글합니다.
     userIcon.addEventListener('click', () => {
-        console.log("start")
         userMenu.classList.toggle('active');
     });
 
@@ -191,7 +190,6 @@ $(document).ready(function () {
 
     // 로그아웃 모달 표시
     $('.item:contains("로그아웃")').on('click', function (event) {
-        console.log("로그아웃 클릭 이벤트 시작");
         event.preventDefault();
         event.stopPropagation();
         $logoutModal.modal('show');
@@ -204,20 +202,16 @@ $(document).ready(function () {
 
     // 비밀번호 재설정 모달 표시
     $('.item:contains("비밀번호 찾기")').on('click', function (event) {
-        console.log("비밀번호 찾기 클릭 이벤트 시작");
         event.preventDefault();
         event.stopPropagation();
         $passwordResetModal.modal('show');
-        console.log("비밀번호 찾기 클릭 이벤트 종료");
     });
 
     // 아이디 찾기 모달 표시
     $('.item:contains("아이디 찾기")').on('click', function (event) {
-        console.log("아이디 찾기 클릭 이벤트 시작");
         event.preventDefault();
         event.stopPropagation(); // 이벤트 버블링 중지
         $usernameFindModal.modal('show');
-        console.log("아이디 찾기 클릭 이벤트 종료");
     });
 
     // 위치 설정 버튼 클릭 시 현재 위치 정보만 가져옴
@@ -240,7 +234,7 @@ $(document).ready(function () {
     $('#setLocation').click(function () {
         handleLocationClick();
         $('#map').remove()
-        $('#profileset').append('<div id="map" style="width:350px; height:300px;"></div>');
+        $('#profileset').append('<div id="map" style="height:300px;"></div>');
         var container = document.getElementById('map'); //지도를 담을 영역의 DOM 레퍼런스
         const setLocation = $('#radiusSelect').val();
 
@@ -297,9 +291,6 @@ $(document).ready(function () {
     $('#signup-setLocation').click(function (event) {
         event.preventDefault();
         handleLocationClick();
-
-        console.log(currentLatitude)
-        console.log(currentLongitude)
 
         if (currentLatitude === undefined && currentLongitude === undefined) {
             handleLocationClick();
@@ -386,12 +377,7 @@ $(document).ready(function () {
                 $("input[name='profile-phoneNumberInput']").val(res.phoneNumber);
                 $("input[name='profile-passwordInput']").val(res.password);
 
-                console.log(res.email)
-                console.log(res.phoneNumber)
-
                 const selectItem = $('.location-input-button-container .menu .item').data();
-
-                console.log(selectItem.value)
 
                 if (res.radius !== selectItem) {
                     $('.location-input-button-container .selected').attr('class', 'item');
@@ -526,9 +512,6 @@ $(document).ready(function () {
         // 아래 코드는 지도 위의 마커를 제거하는 코드입니다
         // marker.setMap(null);
 
-        console.log("latitude : " + currentLatitude)
-        console.log("longitude : " + currentLongitude)
-
         $.ajax({
             url: `/api/users/profile`,
             type: 'PUT',
@@ -547,7 +530,6 @@ $(document).ready(function () {
 
                 // 이미지URL 가져오기
                 const newprofileImage = res.data.profileImage;
-                console.log("profileImage : " + profileImage)
 
                 // 현재 시간을 가져옵니다.
                 const currentTime = new Date();
@@ -578,7 +560,6 @@ $(document).ready(function () {
                 window.location.href = '/';
             },
             error: function (res) {
-                console.log(res)
                 if (res.responseJSON.message === "중복된 전화번호입니다.") {
                     alert("중복된 전화번호입니다.")
                 } else if (res.responseJSON.message === "중복된 이메일입니다.") {
@@ -605,7 +586,6 @@ $(document).ready(function () {
         event.preventDefault();
 
         var phoneNumber = $('#signup-phoneNumberInput').val();
-        console.log("phoneNumber : " + phoneNumber)
         if (!phoneNumber) { // 전화번호가 입력되지 않았을 때
             alert("전화번호를 입력해주세요.");
             return;
@@ -730,7 +710,6 @@ $(document).ready(function () {
                 window.location.href = '/';
             },
             error: function (res) {
-                console.log(res)
                 alert(res.responseText.message)
             }
         });
@@ -804,18 +783,14 @@ $(document).ready(function () {
             contentType: "application/json",
             data: JSON.stringify({username: username, password: password}),
             success: function (res, status, xhr) {
-                console.log(res.data)
-                console.log("status : " + status)
 
                 // HTTP 헤더에서 토큰 가져오기
                 const token = xhr.getResponseHeader("Authorization");
-                console.log("token : " + token)
 
                 // 서버 응답에서 userId, nickname 꺼내오기
                 const userId = res.data.userId;
                 const nickname = res.data.nickname;
                 const profileImage = res.data.profileImage;
-                console.log("profileImage : " + profileImage)
 
                 // 현재 시간을 가져옵니다.
                 const currentTime = new Date();
@@ -963,9 +938,6 @@ $(document).ready(function () {
         // 로그인 상태 UI 업데이트
         $('#login-btn').replaceWith('<li class="welcome-msg">' + nickname + '님 환영합니다.</li>');
 
-        console.log("latitude : " + currentLatitude)
-        console.log("longitude : " + currentLongitude)
-
         if (!email && !phoneNumber && currentLatitude === 0.0 && currentLongitude === 0.0) {
             alert("로그인 성공! 프로필에서 이메일, 전화번호, 위치설정을 바로 설정해주세요!")
         } else if (!email && phoneNumber && currentLatitude !== 0.0 && currentLongitude !== 0.0) {
@@ -999,14 +971,11 @@ $(document).ready(function () {
     // 로그아웃 확인 모달의 확인 버튼 클릭 시 로그아웃 이벤트 실행
     $('#logout-confirm-modal .ui.green.ok.inverted.button').on('click', function () {
         // 확인 버튼을 클릭하면 로그아웃을 실행
-        console.log("로그아웃 시작");
 
         var token = Cookies.get('Authorization');
         if (!token.startsWith("Bearer ")) {
             token = "Bearer " + token;
         }
-
-        console.log("token : " + token);
 
         $.ajax({
             url: "/api/users/logout",
@@ -1027,7 +996,6 @@ $(document).ready(function () {
                 }
             },
             error: function (error) {
-                console.log("로그아웃 종료");
                 alert("로그아웃 요청 실패: " + error.statusText);
             }
         });
@@ -1092,7 +1060,6 @@ $(document).ready(function () {
     $('#deactivationButton').on('click', function (event) {
         event.preventDefault();
         event.stopPropagation(); // 이벤트 버블링 중지
-        console.log("start")
         let phoneNumber = $('#deactive-phoneNumberInput').val();
         let inputCode = $('#deactive-verifyCodeInput').val();
         let email = $('#deactive-emailInput').val();
@@ -1120,8 +1087,6 @@ $(document).ready(function () {
     $('#confirmDeactivationButton').on('click', function () {
         let inputCode = $('#deactive-deactivationCodeInput').val();
         let token = Cookies.get('Authorization');
-        console.log("inputCode : " + inputCode)
-        console.log("token : " + token)
 
         $.ajax({
             beforeSend: function (xhr) {
@@ -1166,7 +1131,6 @@ $(document).ready(function () {
                 alert('인증번호가 전송되었습니다.');
             },
             error: function (error) {
-                console.log(error);
                 alert('오류가 발생했습니다.');
             }
         });
@@ -1216,7 +1180,6 @@ $(document).ready(function () {
         event.preventDefault();
 
         var username = $('#passwordReset-usernameInput').val();
-        console.log("username : " + username)
         if (!username.trim()) {
             alert("아이디를 입력해주세요.");
             return;
@@ -1325,21 +1288,16 @@ $(document).ready(function () {
         // 폼에서 입력한 아이디와 임시 비밀번호 가져오기
         var username = $('input[name="tempLogin-usernameInput"]').val();
         var tempPassword = $('input[name="tempLogin-passwordInput"]').val();
-        console.log("username : " + username)
-        console.log("tempPassword : " + tempPassword)
 
         $.ajax({
             url: `/api/account-recovery/temp-login?` + $.param({username: username, temppassword: tempPassword}),
             type: "POST",
             success: function (res, status, xhr) {
-                console.log("status : " + status)
 
                 // HTTP 헤더에서 토큰 가져오기
                 var token = xhr.getResponseHeader("Authorization");
-                console.log("token : " + token)
 
                 // 서버 응답에서 userId 꺼내오기
-                console.log("tempuser : " + res.data)
                 var userId = res.data.userId;
                 var nickname = res.data.nickname;
                 var profileImage = res.data.profileImage;
